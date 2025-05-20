333cb40e1fb0c909dc7f3ea7c54f323e5157b72a
Franco Vasquez <franco.vasquez9897@gmail.com>
2025-05-20 15:41:01 -0500

    Codificacion Ejercicio 1

--- Program.cs
+++ Program.cs
@@ -6,3 +6,14 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+Console.WriteLine("Ingrese un numero entero");
+string input  = Console.ReadLine();
+int num = int.Parse(input);
+Console.WriteLine($"valor de num:{num}");
+int invertir=0;
+while (num>0)
+{
+    invertir = invertir*10 + num%10;
+    num = num/10;
+}
+Console.WriteLine($"valor de num:{invertir}");
